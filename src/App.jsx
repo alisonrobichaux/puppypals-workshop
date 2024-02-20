df86e5d6f5667f3c9adace1dc6057735bda12c2d
alisonrobichaux <alisonrobichaux@gmail.com>
2024-02-20 10:13:59 -0600

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import './App.css'
+import { useState } from 'react'
+import { puppyList } from './data.js'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPuppy = puppies.find((puppy) => puppy.id === featPupId)
+
+  console.log('featuredPuppy', featuredPuppy)
+
+  // if featPupId === null, something === null
+  // if featPupId !== null, something === "something"
+  const something = featPupId && "something"
 
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
-  )
+    <div className="App">
+      { featPupId &&
+        <div>
+          <h2>{featuredPuppy.name}</h2>
+          <ul>
+            <li>Age: {featuredPuppy.age}</li>
+            <li>Email: {featuredPuppy.email}</li>
+          </ul>
+        </div>
+      }
+      {
+        puppies.map((puppy) => {
+          return <p onClick={() => setFeatPupId(puppy.id)}
+                  key={puppy.id}>{puppy.name}</p>
+        })
+      }
+    </div>
+  );
 }
 
 export default App
